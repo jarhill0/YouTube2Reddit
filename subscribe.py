2a37f0295471d89678f0204759881da822517481
jarhill0 <jarhill0@gmail.com>
2017-02-12 13:15:08 -0800

    added subscribe.py, which can be called in the main directory to "subscribe" to a new channel or user

---
+++ subscribe.py
@@ -0,0 +1,31 @@
+def is_username():
+    user_or_channel = input('User or channel? [u/c]\n').lower()
+    if user_or_channel in ['u', 'user']:
+        return True
+    elif user_or_channel in ['c', 'channel']:
+        return False
+    else:
+        print(
+            'Invalid. Enter U for user or C for channel (channel looks like UC2C_jShtL725hvbm1arSV9w; user looks like cgpgrey).')
+        return is_username()
+
+
+def new_subscription():
+    new_sub = input('Enter channel ID or username.\n')
+    return new_sub.strip()
+
+
+def subscribe():
+    if is_username():
+        with open('config/subbed_users.txt', 'a') as f:
+            f.write(new_subscription() + '\n')
+    else:
+        with open('config/subbed_channels.txt', 'a') as f:
+            f.write(new_subscription() + '\n')
+
+    if input('Add another subscription? [y/n]\n').lower() in ['yes', 'y']:
+        subscribe()
+
+
+print('Subcribe YouTube2Reddit to another channel.')
+subscribe()
