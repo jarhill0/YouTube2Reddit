292d3064e4ce8bda0f83c61dd99c5fbd6c329b6b
jarhill0 <jarhill0@gmail.com>
2017-02-20 23:33:35 -0800

    redefined definition of `subbed_users` and `subbed_channels`. I *think* it works now…

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from time import struct_time
 from time import time
 from datetime import datetime
 
+
 def log_in_to_reddit():
     global reddit
     reddit = praw.Reddit(
@@ -12,22 +13,29 @@ def log_in_to_reddit():
         user_agent='YouTube2Reddit')
 
 
-def get_subbed():
-    global subbed_users
-    global subbed_channels
+def get_subbed_users():
     subbed_users = set()
-    subbed_channels = set()
     with open('config/subbed_users.txt') as f:
         for creator in f.readlines():
             subbed_users.add(creator.strip())
+    return subbed_users
+
+
+def get_subbed_channels():
+    subbed_channels = set()
     with open('config/subbed_channels.txt') as f:
         for creator in f.readlines():
             subbed_channels.add(creator.strip())
+    return subbed_channels
 
 
 def find_epoch(datetime_object):
     epoch = datetime.utcfromtimestamp(0)
     return int((datetime.fromtimestamp(mktime(struct_time(datetime_object))) - epoch).total_seconds())
+
+
+with open('config/last_run.txt') as f:
+    last_run = int(f.read().strip())
 
 
 def get_videos(channel_feed):
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from functions import log_in_to_reddit
-from functions import get_subbed
+from functions import get_subbed_users
+from functions import get_subbed_channels
 from functions import find_epoch
 from functions import get_videos
 from functions import write_last_run
 
 
-with open('config/last_run.txt') as f:
-    last_run = int(f.read().strip())
+
 videos_to_submit = []
-subbed_users = set()
-subbed_channels = set()
+
 with open('config/target_sub.txt') as f:
     target_sub = f.read().strip()
 
-get_subbed()
+subbed_users = get_subbed_users()
+subbed_channels = get_subbed_channels()
+print(subbed_channels)
+print(subbed_users)
 
 for user in subbed_users:
     videos = get_videos('https://www.youtube.com/feeds/videos.xml?user=' + user)
