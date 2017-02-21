f3ea9c43052c0aa96be1b49683ca1b4761b80924
jarhill0 <jarhill0@gmail.com>
2017-02-21 00:08:07 -0800

    alphabetized the wiki update function

--- update_wiki.py
+++ update_wiki.py
@@ -4,17 +4,17 @@ from functions import get_subbed_channels
 
 
 def update_wiki():
-    subbed_users = get_subbed_users()
-    subbed_channels = get_subbed_channels()
+    subbed_users = list(get_subbed_users())
+    subbed_channels = list(get_subbed_channels())
     reddit = log_in_to_reddit()
     with open('config/target_sub.txt') as f:
         target_sub = f.read().strip()
 
     subbed_users_wiki = ''
-    for user in subbed_users:
+    for user in sorted(subbed_users):  # sort is practically essential for readability
         subbed_users_wiki += user + '  \n'
     subbed_channels_wiki = ''
-    for channel in subbed_channels:
+    for channel in sorted(subbed_channels):  # not necessary to sort; but it makes it nice to spot changes
         subbed_channels_wiki += channel + '  \n'
 
     reddit.subreddit(target_sub).wiki.create('subbed_users', subbed_users_wiki)
