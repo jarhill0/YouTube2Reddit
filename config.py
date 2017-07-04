74ba2415e2897c08d8cb226dd55669b783b9002f
jarhill0 <jarhill0@gmail.com>
2017-07-03 23:24:18 -0700

    refactor first_run to save to the wiki

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 sub_name = ''
+
 subbed_users_page_name = 'subbed_users'
 subbed_channels_page_name = 'subbed_channels'
 already_submitted_page_name = 'already_submitted'
+user_base_url = 'https://www.youtube.com/feeds/videos.xml?user='
+channel_base_url = 'https://www.youtube.com/feeds/videos.xml?channel_id='
--- first_run.py
+++ first_run.py
@@ -3,33 +3,27 @@ import functions
 
 def list_old_videos(subbed_users, subbed_channels):
     videos_to_submit = []
-    submitted_this_run = []
 
     for user in subbed_users:
-        videos = functions.get_videos('https://www.youtube.com/feeds/videos.xml?user=' + user)
-        for video in videos:
-            videos_to_submit.append(video)
-
+        videos_to_submit.extend(functions.get_videos(config.user_base_url + user))
     for channel_id in subbed_channels:
-        videos = functions.get_videos('https://www.youtube.com/feeds/videos.xml?channel_id=' + channel_id)
-        for video in videos:
-            videos_to_submit.append(video)
+        videos_to_submit.extend(functions.get_videos(config.channel_base_url + channel_id))
 
-    for video in videos_to_submit:
-        submitted_this_run.append(video['url'])
+    submitted_this_run = [video['id'] for video in videos_to_submit]
+    functions.write_already_submitted(functions.log_in_to_reddit(), submitted_this_run)
 
-    with open('config/already_submitted.txt', 'a') as f:
-        for url in submitted_this_run:
-            f.write(url + '\n')
+
+def main():
+    run_it = input(
+        'WARNING! Running this script means that the bot will not post any videos to Reddit that were'
+        ' posted prior to running this script. This is a good thing if you have never run the bot before,'
+        ' as it will prevent spam, but it is probably a bad thing if you have run the bot before. Do you'
+        ' wish to continue? [y/n] ').lower() in ['y', 'yes']
+    if not run_it:
+        print('Exiting.')
+    else:
+        list_old_videos(functions.get_subbed_users(), functions.get_subbed_channels())
 
 
 if __name__ == '__main__':
-    run_it = input('WARNING! Running this script means that the bot will not post any videos to Reddit that were'
-                   ' posted prior to running this script. This is a good thing if you have never run the bot before,'
-                   ' as it will prevent spam, but it is probably a bad thing if you have run the bot before. Do you'
-                   ' wish to continue? [y/n] ').lower() in ['y', 'yes']
-    if not run_it:
-        print('Exiting.')
-        exit()
-    else:
-        list_old_videos(functions.get_subbed_users(), functions.get_subbed_channels())
+    main()
