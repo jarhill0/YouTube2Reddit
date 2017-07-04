87bd9238f051588c072af6061b1bf0f53c659d58
jarhill0 <jarhill0@gmail.com>
2017-07-03 23:11:05 -0700

    better logic in first_run

--- first_run.py
+++ first_run.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     run_it = input('WARNING! Running this script means that the bot will not post any videos to Reddit that were'
                    ' posted prior to running this script. This is a good thing if you have never run the bot before,'
                    ' as it will prevent spam, but it is probably a bad thing if you have run the bot before. Do you'
-                   ' wish to continue? [y/n] ').lower()
-    if run_it not in ['y', 'yes']:
+                   ' wish to continue? [y/n] ').lower() in ['y', 'yes']
+    if not run_it:
         print('Exiting.')
         exit()
     else:
