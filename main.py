bf8d3a38473e6272fd9f616080a8986863312ce7
jarhill0 <jarhill0@gmail.com>
2017-07-03 23:06:30 -0700

    improve code quality in main.py

--- main.py
+++ main.py
@@ -2,30 +2,23 @@ import config
 import functions
 import praw
 
-if __name__ == '__main__':
+
+def main():
     videos_to_submit = []
-    submitted_this_run = []
+    submitted_this_run = {}
     target_sub = config.sub_name
     reddit = functions.log_in_to_reddit()
-
-    with open('config/already_submitted.txt') as f:
-        already_submitted = set(f.read().split('\n'))
-        already_submitted.remove('')
-
+    already_submitted = functions.get_already_submitted(reddit)
     subbed_users = functions.get_subbed_users(reddit)
     subbed_channels = functions.get_subbed_channels(reddit)
 
     for user in subbed_users:
         videos = functions.get_videos('https://www.youtube.com/feeds/videos.xml?user=' + user)
-        for video in videos:
-            if video['id'] not in already_submitted:
-                videos_to_submit.append(video)
+        videos_to_submit.extend(v for v in videos if v['id'] not in already_submitted)
 
     for channel_id in subbed_channels:
         videos = functions.get_videos('https://www.youtube.com/feeds/videos.xml?channel_id=' + channel_id)
-        for video in videos:
-            if video['id'] not in already_submitted:
-                videos_to_submit.append(video)
+        videos_to_submit.extend(v for v in videos if v['id'] not in already_submitted)
 
     for video in videos_to_submit:
         try:
@@ -35,7 +28,12 @@ if __name__ == '__main__':
                                                 send_replies=False)
         except praw.exceptions.APIException:
             print('Already submitted %s by %s.' % (video['title'], video['author']))
-        else:
-            submitted_this_run.append(video['id'])
+        finally:
+            # Either way, it's already submitted.
+            submitted_this_run.add(video['id'])
 
     functions.write_already_submitted(reddit, submitted_this_run)
+
+
+if __name__ == '__main__':
+    main()
